1f1e9f947ed3ab7e68e47efd6d2dc281a7d55699
Kalla <kallamcreates@gmail.com>
2018-06-10 20:06:52 +0200

    Adding unit test

--- src/test/java/PasswordChecker/AppTest.java
+++ src/test/java/PasswordChecker/AppTest.java
@@ -8,7 +8,83 @@ import org.junit.Test;
 public class AppTest {
 
     @Test
-    public void testCase1() {
+    public void TC1_testCase1() {
         Assert.assertFalse(checkingFUCNTION(""));
     }
+
+    @Test
+    public void TC2_SmallLetters() {
+        Assert.assertFalse(checkingFUCNTION("qwertyuiop"));
+    }
+
+    @Test
+    public void TC3_SmallAndBig() {
+        Assert.assertFalse(checkingFUCNTION("ASDFGjkloiu"));
+    }
+
+    @Test
+    public void TC4_LessThan6() {
+        Assert.assertFalse(checkingFUCNTION("12we"));
+    }
+
+    @Test
+    public void TC5_OneNumber() {
+        Assert.assertFalse(checkingFUCNTION("2"));
+    }
+
+    @Test
+    public void TC6_SpecialChar() {
+        Assert.assertFalse(checkingFUCNTION("!"));
+    }
+
+    @Test
+    public void TC7_OnlyBig() {
+        Assert.assertFalse(checkingFUCNTION("ZXCVB"));
+    }
+
+    @Test
+    public void TC8_oneNumberOneChar() {
+        Assert.assertFalse(checkingFUCNTION("1@"));
+    }
+
+    @Test
+    public void TC9_smallLettersAndMoreThan6() {
+        Assert.assertFalse(checkingFUCNTION("klaudiamar"));
+    }
+
+    @Test
+    public void TC10_NumbersAndLongerThan6() {
+        Assert.assertFalse(checkingFUCNTION("1234567"));
+    }
+
+    @Test
+    public void TC11_SpecialAndMore6() {
+        Assert.assertFalse(checkingFUCNTION("!@!@#$"));
+    }
+
+    @Test
+    public void TC12_SmallBigAndOneNumber() {
+        Assert.assertFalse(checkingFUCNTION("As1"));
+    }
+
+    @Test
+    public void TC13_SmallBigSpecialChar() {
+        Assert.assertFalse(checkingFUCNTION("Zx!"));
+    }
+
+    @Test
+    public void TC14_SmallBigLettersLonger6AndNumber() {
+        Assert.assertFalse(checkingFUCNTION("ASDgjkhb1"));
+    }
+
+    @Test
+    public void TC15_SmallBigLonger6AndSpecial() {
+        Assert.assertFalse(checkingFUCNTION("ZXCasdf!"));
+    }
+
+    @Test
+    public void TC16_StrongPasword() {
+        Assert.assertTrue(checkingFUCNTION("ASxcvb!!1234"));
+    }
 }
+
